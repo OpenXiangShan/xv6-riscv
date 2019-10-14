98eb8b8ccf10dc4cc627aa236ec6a62abed28409
Zihao Yu <yuzihao@ict.ac.cn>
2019-10-14 18:30:08 +0800

    kernel,memlayout: use macro __NEMU__ to support different CLINT address

--- kernel/memlayout.h
+++ kernel/memlayout.h
@@ -26,7 +26,12 @@
 #define VIRTIO0_IRQ 1
 
 // local interrupt controller, which contains the timer.
+#ifdef __NEMU__
+#define CLINT 0xa2000000L
+#else
 #define CLINT 0x2000000L
+#endif
+
 #define CLINT_MTIMECMP(hartid) (CLINT + 0x4000 + 8*(hartid))
 #define CLINT_MTIME (CLINT + 0xBFF8) // cycles since boot.
 
